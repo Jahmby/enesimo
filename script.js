e8c8a11b9a5bdb216d7c3270fd7aad282c8f782f
jah <jamby85@hotmail.com>
2024-10-21 08:40:52 +0200

    Finish practicing class element

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function testFunction() {
-    var x = document.getElementByClassName("city");
+    var x = document.getElementsByClassName("mycontainer");
     for (var i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
