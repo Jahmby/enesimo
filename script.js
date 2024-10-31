e36da5958da73783328e67cf9d6f6bde9bf4f703
jah <jamby85@hotmail.com>
2024-10-31 08:50:28 +0100

    Finished with the id attribute now learning how to merge this branch with main in git

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+
+//Getting an HTML element by his class name and manipulate it with JS 👇
 function testFunction() {
     var x = document.getElementsByClassName("mycontainer");
     for (var i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
+}
+
+//Getting an HTML element by his id name and manipulating it with JS 👇
+function displayResult () {
+    document.getElementById("myHeader").innerHTML = "Have a nice day!";
 }
